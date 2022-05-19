520cd791bc1b01c75bedab32f264404774c492c9
y.fomenkov <y.fomenkov>
2022-05-20 00:44:18 +0300

    Initial commit

---
+++ src/main/java/com/petstore/controllers/ManagePets.java
@@ -0,0 +1,11 @@
+package com.petstore.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ManagePets {
+    //TODO: Get all pets
+    //TODO: Reserve pets
+    //TODO: Remove pets
+    //TODO: Update pet
+}
